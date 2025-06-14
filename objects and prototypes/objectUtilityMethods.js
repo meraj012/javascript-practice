891273374e16daf634c999d54479ba16f4e96876
Md Meraj Ansari <meraj84091@gmail.com>
2025-06-15 00:47:11 +0530

    commited

--- objects and prototypes/objectUtilityMethods.js
+++ objects and prototypes/objectUtilityMethods.js
@@ -13,8 +13,25 @@ console.log(Object.values(person));
 console.log(Object.entries(person));
 
 // Object.assign(target, ...sources)  // copies properties from one or meore source ojects to a target object.
-const target = { a: 1 };
-const source = { b: 2 };
+const source = { a: 1 };
+const target = { b: 2 };
 Object.assign(target, source);
-console.log(target); // {a: 1, b: 2}
-console.log(source);
+console.log(source); // {a: 1}
+console.log(target); // {b: 2, a: 1}
+
+// Object.freeze(obj)  // no changes allow, freeze the object
+const obj = {
+  a: 1,
+};
+Object.freeze(obj);
+obj.b = 2; // can't update
+
+// Object.seal(obj)  // Prevents adding or deleting properties, but allows modifiying existing ones.
+Object.seal(obj);
+delete obj.a; // not allowed
+obj.a = 2; // allowed
+
+// Object.getPrototypeOf(obj)  // returns the prototype of the object
+console.log(Object.getPrototypeOf(person));
+
+// Object.hasOwnProperty(property)
